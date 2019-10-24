c4c7c492a975d9871d0152dd896677d5371b1e78
Jorge <jorgea3004@gmail.com>
2019-10-23 20:53:17 -0500

    ADD se agrega scroll en ventana

--- html_desktop_mate/js/App.js
+++ html_desktop_mate/js/App.js
@@ -44,7 +44,7 @@ class Window extends React.Component {
   state = {
         page:1,
         total:0,
-        limite:20,
+        limite:33,
         elementos:[]
     }
   constructor() {
@@ -69,7 +69,7 @@ class Window extends React.Component {
         });
   }
   addPage(idPage){
-    let count = 20;
+    let count = this.state.limite;
     let fin = count * idPage;
     let inicio = fin - count;
     let elems = this.state.elementos;
@@ -110,4 +110,56 @@ class Window extends React.Component {
   }
 }
 
+/*class App extends React.Component {
+    render() {
+        return (
+            <FileUploadForm />
+        );
+    }
+}*/
+class FileUploadForm extends React.Component {
+
+    UPLOAD_ENDPOINT = './php/upload.php';
+    constructor(props) {
+        super(props);
+        this.state ={
+          file:null
+        }
+        this.onSubmit = this.onSubmit.bind(this)
+        this.onChange = this.onChange.bind(this)
+        this.uploadFile = this.uploadFile.bind(this)
+    }
+    async onSubmit(e){
+        e.preventDefault() 
+        let res = await this.uploadFile(this.state.file);
+        console.log(res.data);
+    }
+    onChange(e) {
+        this.setState({file:e.target.files[0]})
+    }
+    async uploadFile(file){
+        
+
+        const formData = new FormData();
+        
+        formData.append('avatar',file)
+        
+        return  await axios.post(this.UPLOAD_ENDPOINT, formData,{
+            headers: {
+                'content-type': 'multipart/form-data'
+            }
+        });
+      }
+    
+      render() {
+        return (
+          <form onSubmit={ this.onSubmit }>
+            <h1> React File Upload Example</h1>
+            <input type="file" onChange={ this.onChange } />
+            <button type="submit">Upload File</button>
+          </form>
+       )
+      }
+        
+}
 ReactDOM.render(<App/>, document.getElementById("app"))
